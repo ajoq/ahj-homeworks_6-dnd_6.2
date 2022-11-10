d171a9427bd1f5d80da7eb4176092c9487d465af
Konstantin Beloglazov <ajoq@ya.ru>
2022-11-10 13:14:55 +0300

    dev 1

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 import Gallery from './Gallery';
 
 const gallery = new Gallery();
-gallery.addImage('Река', 'https://top10a.ru/wp-content/uploads/2019/11/4-214.jpg');
-gallery.addImage('Олень', 'https://huntland.ru/wp-content/uploads/2018/11/severniy_olen1.jpg');
+gallery.init();
